5c85fa26f7be6609a8ea7294228e0bd065a7ae3a
Robin Gustafsson <git@rgson.se>
2018-01-06 00:54:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ticket Tool
+
+A command-line tool for ticket management (the bug tracker kind).
+Keeps track of tickets, including status, affected files and related notes.
+
+## Getting started
+
+1. Place `ticket.py` somewhere in your `$PATH`, e.g. in `~/bin`.
+2. Run `ticket.py -h` for usage information.
+
+## Dependencies
+
+- Python 3
---
+++ ticket.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+import argparse
+import enum
+import fileinput
+import functools
+import glob
+import itertools
+import os
+import re
+import shutil
+import subprocess
+import sys
+import textwrap
+
+class State(enum.Enum):
+    active = 1
+    archived = 2
+
+    @property
+    def path(self):
+        if self is State.active:
+            return os.path.expanduser('~') + "/tickets/active"
+        if self is State.archived:
+            return os.path.expanduser('~') + "/tickets/archive"
+
+@functools.total_ordering
+class Ticket:
+    def __init__(self, ticket_id):
+        self.id = ticket_id
+        for s in (State.active, State.archived):
+            self.state = s
+            if os.path.exists(self._path):
+                self.notes = Notes(self._notes_filepath)
+                break
+        else:
+            self.state = None
+            self.notes = None
+
+    def create(self):
+        if self.state is not None:
+            print("Ticket {} already exists".format(self.id), file=sys.stderr)
+            return
+        self.state = State.active
+        self.notes = Notes.create(self.id, self._notes_filepath)
+
+    def open(self):
+        if self.state is None:
+            return self._warn_doesnt_exist()
+        directory = self._path
+        notes_file = self._notes_filepath
+        files = self.notes.files if self.notes is not None else {}
+        directories = [directory] + list(self._find_repos(files))
+        terminal_tabs = list(itertools.chain.from_iterable(
+            ['--tab', '--working-directory', d] for d in directories))
+        subprocess.Popen(['gnome-terminal', '--geometry', '80x88+0+0'] + terminal_tabs)
+        subprocess.Popen(['subl', directory, notes_file] + list(files))
+        subprocess.Popen(['nemo', directory])
+
+    def delete(self):
+        directory = self._path
+        if directory is not None:
+            shutil.rmtree(directory)
+
+    def archive(self):
+        self._move(State.archived)
+
+    def unarchive(self):
+        self._move(State.active)
+
+    @property
+    def status(self):
+        if self.state is None:
+            return self._warn_doesnt_exist()
+        return self.notes.status
+
+    @status.setter
+    def status(self, value):
+        if self.state is None:
+            return self._warn_doesnt_exist()
+        self.notes.status = value
+
+    @property
+    def summary(self):
+        if self.state is None:
+            return self._warn_doesnt_exist()
+        return self.notes.summary
+
+    def _move(self, new_state):
+        if self.state is None:
+            return self._warn_doesnt_exist()
+        if self.state is new_state:
+            return
+        old_path = self._path
+        self.state = new_state
+        new_path = self._path
+        shutil.move(old_path, new_path)
+        self.notes.filepath = new_path
+
+    def _warn_doesnt_exist(self):
+        print("Ticket {} does not exist".format(self.id), file=sys.stderr)
+
+    def _find_repos(self, files):
+        dirs = {os.path.dirname(f) for f in files}
+        repos = set()
+        cmd = ['git', 'rev-parse', '--show-toplevel']
+        for d in dirs:
+            with subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=d) as p:
+                repo = p.stdout.read()
+                if repo:
+                    repos.add(repo.decode('utf-8').strip())
+        return repos
+
+    @property
+    def _notes_filepath(self):
+        if self.state is None:
+            return None
+        return "{}/notes.md".format(self._path, self.id)
+
+    @property
+    def _path(self):
+        if self.state is None:
+            return None
+        return "{}/{}".format(self.state.path, self.id)
+
+    @staticmethod
+    def get_tickets(include_archived=False):
+        tickets = set()
+        included_states = {State.active} if not include_archived else {State.active, State.archived}
+        for state in included_states:
+            state_dir = state.path
+            for ticket_dir in glob.glob(state_dir + '/*/'):
+                ticket_id = ticket_dir[len(state_dir)+1:-1]
+                tickets.add(Ticket(ticket_id))
+        return tickets
+
+    def __eq__(self, other):
+        if not hasattr(other, "id"):
+            return NotImplemented
+        return self.id == other.id
+
+    def __lt__(self, other):
+        if not hasattr(other, "id"):
+            return NotImplemented
+        return self.id < other.id
+
+    def __str__(self):
+        return("{:10}  {:25.25}  {}".format(self.id, self.summary, self.status))
+
+    def __hash__(self):
+        return hash(self.id)
+
+class Notes:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    @property
+    def status(self):
+        with open(self.filepath, 'r') as f:
+            for line in f:
+                if line.startswith("_Status_"):
+                    return line[line.index(':') + 1:].strip()
+        return ''
+
+    @status.setter
+    def status(self, value):
+        with fileinput.FileInput(self.filepath, inplace=True) as f:
+            for line in f:
+                if line.startswith("_Status_"):
+                    line = "_Status_ : {}\n".format(value)
+                print(line, end='')
+
+    @property
+    def summary(self):
+        with open(self.filepath, 'r') as f:
+            for line in f:
+                if line.startswith("_Summary_"):
+                    return line[line.index(':') + 1:].strip()
+        return ''
+
+    @property
+    def files(self):
+        files_section = False
+        relevant_files = set()
+        with open(self.filepath, 'r') as f:
+            for line in f:
+                if line.startswith("## Files"):
+                    files_section = True
+                elif line.startswith("## Notes"):
+                    files_section = False
+                elif files_section and line.startswith("- "):
+                    relevant_files.add(line[len("- "):].strip())
+        return relevant_files
+
+    @staticmethod
+    def create(ticket_id, filepath):
+        content = textwrap.dedent("""\
+            # Ticket {}
+
+            _Summary_:
+            _Status_ : New
+
+
+            ## Files
+
+            -
+
+
+            ## Notes
+
+            """.format(ticket_id))
+        os.makedirs(os.path.dirname(filepath))
+        with open(filepath, 'w+') as f:
+            print(content, file=f)
+        return Notes(filepath)
+
+
+################################################################################
+
+# Parse command-line arguments
+
+parser = argparse.ArgumentParser()
+action = parser.add_mutually_exclusive_group(required=False)
+action.add_argument('-l' , '--list'     , help="list tickets"                   , action='store_true')
+action.add_argument('-k' , '--list-all' , help="list tickets, including archive", action='store_true')
+action.add_argument('-a' , '--archive'  , help="move tickets to archive"        , action='store_true')
+action.add_argument('-u' , '--unarchive', help="move tickets from archive"      , action='store_true')
+action.add_argument('-o' , '--open'     , help="open existing tickets only"     , action='store_true')
+action.add_argument('-d' , '--delete'   , help="delete tickets"                 , action='store_true')
+action.add_argument('-s' , '--status'   , help="set the status"                 , nargs=1)
+parser.add_argument('ticket'            , help="the ticket ID(s) to open/change", type=Ticket, nargs='*')
+parser.set_defaults(action='create_tickets')
+args = parser.parse_args()
+
+# Create active and archive directories
+
+for state in {State.active, State.archived}:
+    os.makedirs(state.path, exist_ok=True)
+
+# Run command
+
+if args.list:
+    found = Ticket.get_tickets()
+    for ticket in sorted(found):
+        print(ticket)
+
+elif args.list_all:
+    found = Ticket.get_tickets(include_archived=True)
+    for ticket in sorted(found):
+        print(ticket)
+
+elif args.archive:
+    for ticket in args.ticket:
+        ticket.archive()
+
+elif args.unarchive:
+    for ticket in args.ticket:
+        ticket.unarchive()
+
+elif args.open:
+    for ticket in args.ticket:
+        ticket.open()
+
+elif args.delete:
+    for ticket in args.ticket:
+        confirm = None
+        while confirm not in set('YyNn'):
+            confirm = input('Are you sure you want to delete {}? (y/N): '.format(ticket.id)) or 'N';
+            if confirm in set('Yy'):
+                ticket.delete()
+
+elif args.status:
+    for ticket in args.ticket:
+        ticket.status = args.status[0]
+
+elif args.ticket:
+    for ticket in args.ticket:
+        if ticket.state is None:
+            ticket.create()
+        ticket.open()
+
+else:
+    parser.print_help()
