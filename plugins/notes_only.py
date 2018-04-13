8952671a8582f4855d89844f035b9bb5f7802ffd
Robin Gustafsson <git@rgson.se>
2018-04-13 21:15:16 +0200

    Create a 'notes only' filter
    Opens only files titled 'notes.md' (like the automatic notes file)

---
+++ plugins/notes_only.py
@@ -0,0 +1,22 @@
+# (c) 2018, Robin Gustafsson <robin@rgson.se>
+#
+# This file is part of Ticklet.
+#
+# Ticklet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ticklet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ticklet.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+
+def filter_files(files, dirs):
+    return [f for f in files if os.path.basename(f) == 'notes.md'], []
