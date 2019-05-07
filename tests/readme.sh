de7b3db1c27df178716212285ce6fedf24a44cd7
Robin Gustafsson <robin@rgson.se>
2019-05-07 22:39:13 +0200

    Convert tests from bash to sh

--- tests/readme.sh
+++ tests/readme.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 ticklet=${1:-'./ticklet.py'}
 export HOME=$(mktemp -d)
 
-diff \
-	<($ticklet -h) \
-	<(sed '/## Usage/,/## Installation/!d' README.md |
-		sed '/```/,/```/!d' | sed '1d;$d')
+# Actual help message
+tmp_a=$(mktemp)
+$ticklet -h >$tmp_a
+
+# Help message in README
+tmp_b=$(mktemp)
+sed '/## Usage/,/## Installation/!d' README.md |
+	sed '/```/,/```/!d' | sed '1d;$d' >$tmp_b
+
+# Must match
+diff $tmp_a $tmp_b
